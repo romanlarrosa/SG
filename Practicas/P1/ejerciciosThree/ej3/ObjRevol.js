1f02d9425813bea4d09f6b57225818161291440b
romanlarrosa <58295696+romanlarrosa@users.noreply.github.com>
2020-03-06 12:10:24 +0100

    Update ObjRevol.js

--- Practicas/P1/ejerciciosThree/ej3/ObjRevol.js
+++ Practicas/P1/ejerciciosThree/ej3/ObjRevol.js
@@ -3,8 +3,8 @@ class ObjRevol extends THREE.Object3D {
         super();
         this.puntos = _puntos;
 
-        var Geom = new THREE.LatheGeometry(_puntos, 3, 0, 1);
-        this.Obj = new THREE.Mesh(Geom, _material);
+        this.Geom = new THREE.LatheGeometry(_puntos, 3, 0, 1);
+        this.Obj = new THREE.Mesh(this.Geom, _material);
         this.Obj.position.set(0,2,0);
 
         this.add(this.Obj);
@@ -12,12 +12,13 @@ class ObjRevol extends THREE.Object3D {
     }
 
 
-    update() {
+    update(resolucion, angulo) {
       // Con independencia de cómo se escriban las 3 siguientes líneas, el orden en el que se aplican las transformaciones es:
       // Primero, el escalado
       // Segundo, la rotación en Z
       // Después, la rotación en Y
       // Luego, la rotación en X
       // Y por último la traslación
+      this.Geom = new THREE.LatheGeometry(this.puntos, resolucion, 0, angulo);
     }
 }
