fbbf9d553e4824357a8db08c058e1384e06099bf
romanlarrosa <58295696+romanlarrosa@users.noreply.github.com>
2020-03-11 10:10:39 +0100

    Update Pie.js

--- Practicas/P1/ejerciciosThree/ej4/Pie.js
+++ Practicas/P1/ejerciciosThree/ej4/Pie.js
@@ -2,11 +2,15 @@ class Pie extends THREE.Object3D {
     constructor(gui, titleGui, _material) {
         super();
         this.puntos = [];
-        this.puntos.push(new THREE.Vector3(0.0, 3.0, 0.0));
-        this.puntos.push(new THREE.Vector3(0.5, 3.0, 0.0));
-        this.puntos.push(new THREE.Vector3(0.5, 0.5, 0.0));
-        this.puntos.push(new THREE.Vector3(2.0, 0.0, 0.0));
         this.puntos.push(new THREE.Vector3(0.0, 0.0, 0.0));
+        this.puntos.push(new THREE.Vector3(1.5, 0.0, 0.0));
+        this.puntos.push(new THREE.Vector3(0.2, 0.5, 0.0));
+        this.puntos.push(new THREE.Vector3(0.2, 3.5, 0.0));
+        this.puntos.push(new THREE.Vector3(0.0, 3.5, 0.0));
+        
+        
+        
+        
 
         this.Geom = new THREE.LatheGeometry(this.puntos, 20, 0, 2*Math.PI);
         this.objeto = new THREE.Mesh(this.Geom, _material);
